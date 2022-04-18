0badbaf4967d52b15e3ad2108c418d0022ebc0e1
Michal Mildner <mm0070@gmail.com>
2022-04-18 15:34:17 +0100

    Add v5l model and update detect.py so that its working

--- detect.py
+++ detect.py
@@ -1,60 +1,120 @@
 import torch
 import cv2
-
-"""
-The function below identifies the device which is availabe to make the prediction and uses it to load and infer the frame. Once it has results it will extract the labels and cordinates(Along with scores) for each object detected in the frame.
-"""
+from time import time
+import numpy as np
 
 
-def score_frame(frame, model):
-    model.to("cpu")
-    frame = [torch.tensor(frame)]
-    results = self.model(frame)
-    labels = results.xyxyn[0][:, -1].numpy()
-    cord = results.xyxyn[0][:, :-1].numpy()
+def score_frame(model, device, frame):
+    """
+    Takes a single frame as input, and scores the frame using yolo5 model.
+    :param frame: input frame in numpy/list/tuple format.
+    :return: Labels and Coordinates of objects detected by model in the frame.
+    """
+    model.to(device)
+    frame = [frame]
+    results = model(frame)
+    labels, cord = results.xyxyn[0][:, -
+                                    1].cpu().numpy(), results.xyxyn[0][:, :-1].cpu().numpy()
     return labels, cord
 
 
-"""
-The function below takes the results and the frame as input and plots boxes over all the objects which have a score higer than our threshold.
-"""
+def class_to_label(model, x):
+    """
+    For a given label value, return corresponding string label.
+    :param x: numeric label
+    :return: corresponding string label
+    """
+    classes = model.names
+    return classes[int(x)]
 
 
-def plot_boxes(self, results, frame):
+def plot_boxes(results, frame, model, min_confidence=0.05):
+    """
+    Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
+    :param results: contains labels and coordinates predicted by model on the given frame.
+    :param frame: Frame which has been scored.
+    :return: Frame with bounding boxes and labels ploted on it.
+    """
     labels, cord = results
     n = len(labels)
     x_shape, y_shape = frame.shape[1], frame.shape[0]
     for i in range(n):
         row = cord[i]
-        # If score is less than 0.2 we avoid making a prediction.
-        if row[4] < 0.2:
-            continue
-        x1 = int(row[0] * x_shape)
-        y1 = int(row[1] * y_shape)
-        x2 = int(row[2] * x_shape)
-        y2 = int(row[3] * y_shape)
-        bgr = (0, 255, 0)  # color of the box
-        classes = self.model.names  # Get the name of label index
-        label_font = cv2.FONT_HERSHEY_SIMPLEX  # Font for the label.
-        cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)  # Plot the boxes
-        cv2.putText(
-            frame, classes[labels[i]], (x1, y1), label_font, 0.9, bgr, 2
-        )  # Put a label over box.
-        return frame
+        if row[4] >= min_confidence:
+            x1, y1, x2, y2 = (
+                int(row[0] * x_shape),
+                int(row[1] * y_shape),
+                int(row[2] * x_shape),
+                int(row[3] * y_shape),
+            )
+            bgr = (0, 255, 0)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+            cv2.putText(
+                frame,
+                class_to_label(model, labels[i]) + " " + str(format(row[4], '.2f')),
+                (x1, y1),
+                cv2.FONT_HERSHEY_COMPLEX,
+                0.9,
+                bgr,
+                2,
+            )
 
+    return frame
 
 def main():
+    # OpenCV capture setup
+    camera_ip = "rtsp://vincent:goodboy@192.168.1.229/stream1"
+    # player = cv2.VideoCapture(camera_ip)  # Tapo
+    # player = cv2.VideoCapture(0)  # MacBook webcam
+    player = cv2.VideoCapture("vincent_compilation.mp4")  # File
+    assert player.isOpened()
+
+    # Get image size details
+    x_shape = int(player.get(cv2.CAP_PROP_FRAME_WIDTH))
+    y_shape = int(player.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    four_cc = cv2.VideoWriter_fourcc(*"MJPG")
+
+    # Load my magnificent model
     model = torch.hub.load(
         "ultralytics/yolov5",
         "custom",
-        path="vincent-epochs-20-batch-16/weights/best.pt",
+        path="vincent-e20-b16-v5l.pt",
     )
 
-    camera_ip = "rtsp://vincent:goodboy@192.168.1.229/stream1"
-    # player = cv2.VideoCapture(camera_ip)  # tapo
-    # player = cv2.VideoCapture(0)  # macbook
-    player = cv2.VideoCapture("vincent_compilation.mp4")  # file
-    assert player.isOpened()
+    # Cross fingers and hope cuda is working
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if device == 'cpu':
+        print("Running on CPU, CUDA is broken!")
+
+    # Window display
+    while player.isOpened():
+        # Capture frame-by-frame
+        ret, frame = player.read()
+        if ret == True:
+            start_time = time()
+            results = score_frame(model, device, frame)
+            frame = plot_boxes(results, frame, model)
+
+            end_time = time()
+            fps = 1 / np.round(end_time - start_time, 3)
+            print(f"Frames Per Second : {fps}")
+
+            # Display the resulting frame
+            cv2.imshow("Frame", frame)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord("q"):
+                break
+
+        # Break the loop
+        else:
+            break
+
+    # When everything is done, release the video capture object
+    player.release()
+
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
